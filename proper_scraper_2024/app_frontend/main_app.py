11fa395551100f6c891549f96f5103b7d2bdb1a3
sooworrow <suvoraum@gmail.com>
2024-11-03 06:32:04 +0100

    create the main instance of the application.In the file proper_scraper_2024/app_frontend/main_app.py , create the main instance of the application and the code block for its launching.

--- proper_scraper_2024/app_frontend/main_app.py
+++ proper_scraper_2024/app_frontend/main_app.py
@@ -1,2 +1,18 @@
-print("Hello Git!")
-print("Second commit!")
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setApplicationName("Scraper")
+
+    scrApp = MainWindow(app)
+
+    scrApp.show()
+
+    try:
+        sys.exit(app.exec())
+    except SystemExit:
+        print('Closing Application')
